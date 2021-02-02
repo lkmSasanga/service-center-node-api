55584fb2baf00f12843a051e0319217c9d001e1b
lkmSasanga <lkmsasanga@gmail.com>
2021-02-02 22:19:23 +0530

    service agent model created

---
+++ api/models/VehicleModel.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let VehicleModelSchema = new Schema({
+    model: {
+        type: String,
+        required: [true, 'Model field is required!']
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const VehicleModel = mongoose.model('ServiceTag', VehicleModelSchema);
+module.exports = { VehicleModel }
+
+
