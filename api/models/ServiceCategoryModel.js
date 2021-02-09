bd4d7173a6212bf65a95b6a7665032f9a8ab719f
lkmSasanga <lkmsasanga@gmail.com>
2021-02-09 17:37:59 +0530

    serviceCategory model modified

--- api/models/ServiceCategoryModel.js
+++ api/models/ServiceCategoryModel.js
@@ -5,15 +5,13 @@ let Schema = mongoose.Schema;
 let ServiceCategoryModelSchema = new Schema({
     category: {
         type: String,
-        // enum: ['full_service', 'body_wash'],
+        enum: ['full_service', 'body_wash'],
         required: [true, 'Title field is required!']
     },
-
     cost: {
         type: String,
         required: [true, 'Cost field is required!']
     },
-
     created_date: {
         type: Date,
         default: Date.now
