0f78ecdf5c6828df5bc49cd2d5b444c697fe2290
lkmSasanga <lkmsasanga@gmail.com>
2021-02-11 13:53:06 +0530

    checking auto deployment

--- app.js
+++ app.js
@@ -37,9 +37,6 @@ app.use(function(req, res) {
     res.status(404).send({ url: req.originalUrl + ' not found' })
 });
 
-
-
-
 app.listen(port, () => {
     console.log(`API server started on: ${port}`);
 });
